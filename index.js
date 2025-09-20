a77c21906b63745ea00fc06f9d91a1adcb35282a
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-09-20 22:48:25 +0530

    TICTACTOE

---
+++ index.js
@@ -0,0 +1,6 @@
+const boxes=document.querySelectorAll(".box");
+const gameInfo=document.querySelector(".game-info")
+const newGameBtn=document.querySelector(".btn");
+
+let currentPlayer;
+let gameGrid;
